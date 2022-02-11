b037dc8316fb4126f4211bb83e111fa8384e4b41
tianhe1986 <w1s2j3229@163.com>
2022-02-11 11:50:19 +0800

    sub and div.
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- action/cal.go
+++ action/cal.go
@@ -1,9 +1,23 @@
 package action
 
+import "errors"
+
 func Add(a int, b int) int {
 	return a + b
 }
 
+func Sub(a int, b int) int {
+	return a - b
+}
+
 func Mul(a int, b int) int {
 	return a * b
+}
+
+func Div(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("math: dividend can not be zero")
+	}
+
+	return a / b, nil
 }
--- action/cal_test.go
+++ action/cal_test.go
@@ -1,31 +1,58 @@
 package action
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAdd(t *testing.T) {
 	t.Run("pos", func(t *testing.T) {
-		if ans := Add(1, 2); ans != 3 {
-			t.Errorf("1 + 2 expected be 3, but %d got", ans)
-		}
-
+		assert.Equal(t, 3, Add(1, 2))
 	})
 	t.Run("neg", func(t *testing.T) {
-		if ans := Add(2, -3); ans != -1 {
-			t.Errorf("2 + -3 expected be -1, but %d got", ans)
-		}
+		assert.Equal(t, -1, Add(2, -3))
+	})
+}
+
+func TestSub(t *testing.T) {
+	t.Run("pos", func(t *testing.T) {
+		assert.Equal(t, 13, Sub(52, 39))
+	})
+	t.Run("neg", func(t *testing.T) {
+		assert.Equal(t, -154, Sub(-12, 142))
 	})
 }
 
 func TestMul(t *testing.T) {
+	// 使用t.Errorf报错
 	t.Run("pos", func(t *testing.T) {
 		if ans := Mul(2, 3); ans != 6 {
 			t.Errorf("2 * 3 expected be 6, but %d got", ans)
 		}
-
 	})
 	t.Run("neg", func(t *testing.T) {
 		if ans := Mul(2, -3); ans != -6 {
 			t.Errorf("2 * -3 expected be -6, but %d got", ans)
 		}
+	})
+}
+
+func TestDiv(t *testing.T) {
+	t.Run("err", func(t *testing.T) {
+		ans, err := Div(6, 0)
+		assert.NotNil(t, err)
+		assert.Equal(t, 0, ans)
+		assert.Equal(t, "math: dividend can not be zero", err.Error())
+	})
+	t.Run("pos", func(t *testing.T) {
+		ans, err := Div(12, 4)
+		assert.Nil(t, err)
+		assert.Equal(t, 3, ans)
+	})
+	t.Run("neg", func(t *testing.T) {
+		ans, err := Div(-32, 4)
+		assert.Nil(t, err)
+		assert.Equal(t, -8, ans)
 	})
 }
